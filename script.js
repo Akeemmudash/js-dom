57215ebda9204bd0f7580f82fd5e462b742b7218
Akeem Mudashiru <akeemmudash@gmail.com>
2026-03-28 18:33:46 +0100

    complete employees app

--- script.js
+++ script.js
@@ -6,68 +6,120 @@ const addEmployeeBtn = document.getElementById("add-employee-btn");
 const tableBody = document.getElementById("employee-table-body");
 const employeeCount = document.getElementById("total-employees");
 
-const employees = [];
+let employees = getPersistedEmployees() || [];
 let id = 1;
 
+employeeCount.innerText = 0;
+function disableInputFields() {
+  addEmployeeBtn.disabled = "true";
+}
+
+const inputFields = document.querySelectorAll("input");
+const inputFieldsArrays = Array.from(inputFields);
+
+inputFieldsArrays.forEach(
+  (field) =>
+    (field.oninput = (e) => {
+      if (
+        inputFieldsArrays.every((inputField) => {
+          if (inputField.value.trim()) return true;
+        })
+      ) {
+        addEmployeeBtn.removeAttribute("disabled");
+      } else {
+        disableInputFields();
+      }
+    }),
+);
+renderEmployees(employees);
 function addEmployee() {
   const nameValue = employeeNameInput.value;
   const deptValue = employeeDeptInput.value;
   const salaryValue = employeeSalaryInput.value;
 
-  console.log(nameValue, deptValue, salaryValue);
+  if (!nameValue || !deptValue || !salaryValue) return;
+
   const employeeObject = {
     name: nameValue,
     dept: deptValue,
     salary: salaryValue,
     id: id,
   };
-  console.log("employeeObject", employeeObject);
   employees.push(employeeObject);
-
   id++;
-
-  tableBody.innerHTML = null;
-
-  employees.forEach((employee) => {
-    const tableRowEl = document.createElement("tr");
-    const employeeNameEl = document.createElement("td");
-    const employeeDeptEl = document.createElement("td");
-    const employeeSalaryEl = document.createElement("td");
-    const actionBtnWrapperEl = document.createElement("td");
-    const actionBtnEl = document.createElement("button");
-    employeeNameEl.innerText = employee.name;
-    employeeDeptEl.innerText = employee.dept;
-    employeeSalaryEl.innerText = employee.salary;
-
-    actionBtnEl.classList.add(
-      "btn",
-      "btn-danger",
-      "btn-sm",
-      "delete-employee-btn",
-    );
-    actionBtnEl.innerText = "Remove";
-    actionBtnEl.onclick = () => remove(employee.id);
-    actionBtnWrapperEl.append(actionBtnEl);
-    tableRowEl.append(
-      employeeNameEl,
-      employeeDeptEl,
-      employeeSalaryEl,
-      actionBtnWrapperEl,
-    );
-
-    tableBody.append(tableRowEl);
-  });
+  persistEmployees(employees);
+  renderEmployees(employees);
+  employeeNameInput.value = "";
+  employeeDeptInput.value = "";
+  employeeSalaryInput.value = "";
+  disableInputFields();
+  employeeCount.innerText = employees.length;
 }
 
-function remove(employeeId) {
-  //generate an array of employee elements with exception to the employee with an id of 1 from the employee array
-  // and log the new array to the console.
+function removeEmployee(employeeId) {
+  console.log("employeeId", employeeId);
+  const newEmployees = employees.filter(
+    (employee) => employee.id !== employeeId,
+  );
+  employees = newEmployees;
+  console.log("newEmployees", newEmployees);
+  //   persistEmployees(newEmployees);
+  renderEmployees(newEmployees);
+  employeeCount.innerText = employees.length;
 }
 addEmployeeBtn.onclick = addEmployee;
-const employeeArr = [
-  { name: "John", dept: "Accounting", salary: "400000", id: 1 },
-  { name: "Steven", dept: "IT", salary: "400000", id: 2 },
-  { name: "Sadiq", dept: "Business", salary: "700000", id: 3 },
-];
 
 //generate a new array from the employeeArr with exception of the Element with an id of 2
+
+function renderEmployees(employeeArray) {
+  const tableRowEl = document.createElement("tr");
+  if (employeeArray.length === 0) {
+    tableBody.innerHTML = null;
+    const tableDataEl = document.createElement("td");
+    tableDataEl.colSpan = 4;
+    tableDataEl.innerText = "No employees have been added";
+    tableDataEl.classList.add("text-center");
+    tableRowEl.append(tableDataEl);
+    tableBody.append(tableRowEl);
+  } else {
+    tableBody.innerHTML = null;
+    employeeArray.forEach((employee) => {
+      const tableRowEl = document.createElement("tr");
+      const employeeNameEl = document.createElement("td");
+      const employeeDeptEl = document.createElement("td");
+      const employeeSalaryEl = document.createElement("td");
+      const actionBtnWrapperEl = document.createElement("td");
+      const actionBtnEl = document.createElement("button");
+      employeeNameEl.innerText = employee.name;
+      employeeDeptEl.innerText = employee.dept;
+      employeeSalaryEl.innerText = employee.salary;
+
+      actionBtnEl.classList.add(
+        "btn",
+        "btn-danger",
+        "btn-sm",
+        "delete-employee-btn",
+      );
+      actionBtnEl.innerText = "Remove";
+      actionBtnEl.onclick = () => removeEmployee(employee.id);
+      actionBtnWrapperEl.append(actionBtnEl);
+      tableRowEl.append(
+        employeeNameEl,
+        employeeDeptEl,
+        employeeSalaryEl,
+        actionBtnWrapperEl,
+      );
+      tableBody.append(tableRowEl);
+    });
+  }
+}
+
+function persistEmployees(employee) {
+  const employeeJSONString = JSON.stringify(employee);
+  localStorage.setItem("employee-data", employeeJSONString);
+}
+
+function getPersistedEmployees(employee) {
+  const employeeJSONString = localStorage.getItem("employee-data");
+  return JSON.parse(employeeJSONString);
+}
